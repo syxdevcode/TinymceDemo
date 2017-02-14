f58bd5cd9bd355d0fac19f1cf772b53f388a01e8
syxtest01@gmail.com <xing_*666888>
2017-02-14 14:52:03 +0800

    新建项目

---
+++ TinymceDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinymceDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ContentResult Upload(HttpPostedFileBase Filedata)
+        {
+            string result = string.Empty;
+            string folder = Server.MapPath("~/File/");
+            string time = DateTime.Now.ToString("yyyyMMddHHmmssff");//获取时间
+            string extension = System.IO.Path.GetExtension(Filedata.FileName);//获取扩展名
+            string newFileName = time + extension;//重组成新的文件名
+            if (!System.IO.Directory.Exists(folder))
+                System.IO.Directory.CreateDirectory(folder);
+
+            Filedata.SaveAs(folder + "\\" + newFileName);
+
+            return Content(newFileName);
+        }
+    }
+}
