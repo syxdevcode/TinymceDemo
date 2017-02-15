8bdea4bc8c1fa4e42c0b3f1dfeab8be54aa3f9a3
syxtest01@gmail.com <xing_*666888>
2017-02-15 09:38:03 +0800

    完善上传图片功能

--- TinymceDemo/Controllers/HomeController.cs
+++ TinymceDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TinymceDemo.Models;
 
 namespace TinymceDemo.Controllers
 {
@@ -28,6 +29,36 @@ namespace TinymceDemo.Controllers
             Filedata.SaveAs(folder + "\\" + newFileName);
 
             return Content(newFileName);
+        }
+
+        /// <summary>
+        /// 接收tinymce插件的内容
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public ActionResult Test(TinyMCEModels model)
+        {
+            var path = Server.MapPath("~/File/123.txt");
+            var str = System.IO.File.ReadAllText(path);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+
+            System.IO.File.WriteAllText(path, model.content);
+
+            return RedirectToAction("Show");
+        }
+
+        /// <summary>
+        /// 将记事本的内容读出来，重新加载到页面上
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Show()
+        {
+            var str = System.IO.File.ReadAllText(Server.MapPath("~/File/123.txt"));
+            ViewBag.str = str.Trim();
+            return View();
         }
     }
 }
---
+++ TinymceDemo/Models/TinyMCEModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinymceDemo.Models
+{
+    public class TinyMCEModels
+    {
+        [AllowHtml]
+        public string content { get; set; }
+    }
+}
